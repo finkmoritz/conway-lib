df8c6327f37b54f989f474e23b2da62d25249dba
moritz <fink.moritz@gmail.com>
2020-08-03 22:09:48 +0200

    Add statistics example

---
+++ example/mcts_bot_statistics.dart
@@ -0,0 +1,31 @@
+import 'package:conway/conway.dart';
+
+void main() {
+  int createCell = 0;
+  int destroyFriendlyCell = 0;
+  int destroyHostileCell = 0;
+  int iterations = 1000;
+
+  for(int i=0; i<iterations; i++) {
+    print('Iteration ${i+1} of $iterations:');
+    Game game = new RandomGame(numberOfPlayers: 2, width: 5,
+        height: 5, fractionLivingCells: 0.65, fractionDeadCells: 0.25);
+    MctsBot mctsBot = new MctsBot(game, iterations: 25000, maxPlayoutDepth: 50);
+    int move = mctsBot.computeRankedNodes()[0].toggledCellID;
+    Cell cell = game.board.getCell(move);
+    switch(cell.state) {
+      case CellState.ALIVE:
+        if(cell.playerID == game.currentPlayer) {
+          destroyFriendlyCell++;
+        } else {
+          destroyHostileCell++;
+        }
+        break;
+      case CellState.DEAD:
+        createCell++;
+        break;
+    }
+    print('Create / Destroy friendly / Destroy hostile: '
+        '$createCell / $destroyFriendlyCell / $destroyHostileCell');
+  }
+}
